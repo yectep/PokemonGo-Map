409bdfc67596e404c9f94a8298010a29831175a5
Chester Li <chester.li@panasonic.aero>
2016-07-27 18:05:43 -0700

    Add pathname to geolocation service for those not on server root

--- static/map.js
+++ static/map.js
@@ -1164,13 +1164,13 @@ $(function() {
   window.setInterval(function() {
     if (navigator.geolocation && Store.get('geoLocate')) {
       navigator.geolocation.getCurrentPosition(function(position) {
-        var baseURL = location.protocol + "//" + location.hostname + (location.port ? ":" + location.port : "");
+        var baseURL = location.protocol + "//" + location.hostname + (location.port ? ":" + location.port : "") + location.pathname;
         var lat = position.coords.latitude;
         var lon = position.coords.longitude;
 
         //the search function makes any small movements cause a loop. Need to increase resolution
         if (getPointDistance(marker.getPosition(), (new google.maps.LatLng(lat, lon))) > 40) {
-          $.post(baseURL + "/next_loc?lat=" + lat + "&lon=" + lon).done(function() {
+          $.post(baseURL + "next_loc?lat=" + lat + "&lon=" + lon).done(function() {
             var center = new google.maps.LatLng(lat, lon);
             map.panTo(center);
             marker.setPosition(center);
